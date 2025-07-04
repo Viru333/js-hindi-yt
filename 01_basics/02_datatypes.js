7e99ce472dd05409fecbb667208374a410904fd5
AMAN  KUMAR YADAV <139741392+Viru333@users.noreply.github.com>
2025-07-04 06:38:13 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3)  // code readability should be high
+
+console.log("Aman")
+
+
+let name = "Aman"
+let age = 21
+let isLockedIn = false;
+
+
+// number  => -(2^53 - 1) to 2^53 - 1
+// bigint  
+// string => ""
+// boolean => true/false
+// null => standalone value/represantation of empty value
+// undefined => value is not assigned
+// symbol => unique 
+
+
+// object
+
+console.log(typeof("Aman")); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
