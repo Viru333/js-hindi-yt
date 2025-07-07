52c641f73141e1924804a7643a01adf2027f1385
AMAN  KUMAR YADAV <139741392+Viru333@users.noreply.github.com>
2025-07-07 06:18:46 +0000

    Comparison operators

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// console.log(null > 0); // false
+// console.log(null < 0); // false
+// console.log(null >= 0); // true , beacuse equality op(==) works differently than comparison op(<,>,<=,>=), where comparison op converts null to a num.
+
+// console.log(undefined == 0); // false
+// console.log(undefined > 0);  // false
+// console.log(undefined < 0);  // false
+
+// Strict check(===) checks datatype also along with value
+
+// console.log("2" === 2);
