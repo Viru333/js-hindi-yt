89f65d6b2528d6f712cb4fe3a997bdfc8eb03787
AMAN  KUMAR YADAV <139741392+Viru333@users.noreply.github.com>
2025-07-07 06:27:06 +0000

    datatypes summary

---
+++ 01_basics/datatypesSummary.js
@@ -0,0 +1,22 @@
+// Primitive :  
+                // String
+                // Number
+                // Boolean
+                // Null
+                // undefined
+                // Symbol
+                // BigInt
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+const bigNumber = 63642424524524278643n
+
+// Reference (Non Primitive)
+                // Array
+                // Objects
+                // Funcions
+
