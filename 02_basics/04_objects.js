4a21eb6e5b5cff2cd88ac542a7af8af70b35c565
AMAN  KUMAR YADAV <139741392+Viru333@users.noreply.github.com>
2025-08-15 17:24:59 +0000

    objects revisit

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,55 @@
+// const tinderUSer = new Object() // Singelton
+
+const tinderUser= {}
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email : "Some@gmail.com",
+    fullname : {
+        userfullname: {
+            firstname : "Aman",
+            lastname : "Kumar"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname);
+
+const obj1 = {1:"a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = { obj1, obj2}
+
+// const obj3 = Object.assign({}, obj1, obj2)
+
+// const obj3 = {...obj1, ...obj2}
+
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email:"aman@gmail.com"
+    },
+    {
+        id: 1,
+        email:"aman@gmail.com"
+    },
+    {
+        id: 1,
+        email:"aman@gmail.com"
+    },
+]
+
+// console.log(users[1].email);
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
