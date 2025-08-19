329ca9e67f5e7b6c1f4a1b7b282f2ef928d3e6c0
AMAN  KUMAR YADAV <139741392+Viru333@users.noreply.github.com>
2025-08-19 07:28:32 +0000

    functions with objects

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -27,5 +27,34 @@ function loginUserMessage(username="sam"){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("Aman"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage("Aman"))
+// console.log(loginUserMessage())
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200,400,500,2000))
+
+const user = {
+    username: "hitesh",
+    price:199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+}
+
+handleObject(user)
+handleObject({
+    username: "sam",
+    price:399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200, 300, 500, 100]))
